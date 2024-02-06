d1ef9aa7a8fbb47d31c2d0b952a3798ee4df7c6a
Oguragou <88374126+ogguraaaaa@users.noreply.github.com>
2024-02-06 11:35:01 +0900

    Add files via upload

---
+++ tainan/scripts/script.js
@@ -0,0 +1,42 @@
+'use strict'
+
+function openModal() {
+  document.getElementById('modal1').style.display = 'block';
+  document.getElementById('overlay1').style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('modal1').style.display = 'none';
+  document.getElementById('overlay1').style.display = 'none';
+}
+
+function openModal2() {
+  document.getElementById('modal2').style.display = 'block';
+  document.getElementById('overlay2').style.display = 'block';
+}
+
+function closeModal2() {
+  document.getElementById('modal2').style.display = 'none';
+  document.getElementById('overlay2').style.display = 'none';
+}
+
+function openModal3() {
+  document.getElementById('modal3').style.display = 'block';
+  document.getElementById('overlay3').style.display = 'block';
+}
+
+function closeModal3() {
+  document.getElementById('modal3').style.display = 'none';
+  document.getElementById('overlay3').style.display = 'none';
+}
+
+function openModal4() {
+  document.getElementById('modal4').style.display = 'block';
+  document.getElementById('overlay4').style.display = 'block';
+}
+
+function closeModal4() {
+  document.getElementById('modal4').style.display = 'none';
+  document.getElementById('overlay4').style.display = 'none';
+}
+
